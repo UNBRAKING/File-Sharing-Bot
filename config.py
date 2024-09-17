a331095043806b4a8f1ea787991ca0040a4b6127
UNBRAKING THINGS <code.pbail@gmail.com>
2024-09-17 20:29:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "26614080"))
 API_HASH = os.environ.get("API_HASH", "7d2c9a5628814e1430b30a1f0dc0165b")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002489666533"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002008957712"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5606990991"))
